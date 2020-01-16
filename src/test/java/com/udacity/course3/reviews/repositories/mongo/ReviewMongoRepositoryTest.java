bd960658b239d67b66448440bef9bf1526df8a88
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-01-16 17:41:32 -0500

    Add more tests for mongo repo

--- src/test/java/com/udacity/course3/reviews/repositories/mongo/ReviewMongoRepositoryTest.java
+++ src/test/java/com/udacity/course3/reviews/repositories/mongo/ReviewMongoRepositoryTest.java
@@ -2,12 +2,19 @@ package com.udacity.course3.reviews.repositories.mongo;
 
 import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.DBObject;
+import com.udacity.course3.reviews.persistence.documents.ReviewDocument;
+import com.udacity.course3.reviews.persistence.entities.Product;
+import com.udacity.course3.reviews.persistence.entities.Review;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,5 +46,43 @@ public class ReviewMongoRepositoryTest {
             .containsOnly("value");
     }
 
+    @Test
+    public void testCreateReview() {
+        Review review = getReview(1);
+        ReviewDocument reviewDocument = reviewMongoRepository.save(new ReviewDocument(review));
+        assertThat(reviewDocument).isNotNull();
+        assertThat(reviewDocument.getJpaId()).isEqualTo(review.getId());
+        assertThat(reviewDocument.getTitle()).isEqualTo(review.getTitle());
+        assertThat(reviewDocument.getContent()).isEqualTo(review.getContent());
+        assertThat(reviewDocument.getDatePosted()).isEqualTo(review.getDatePosted());
+        assertThat(reviewDocument.getProductId()).isEqualTo(review.getProduct().getId());
+    }
+
+    @Test
+    public void testFindAll() {
+        final int numReviews = 10;
+        List<Review> reviews = new ArrayList<>(numReviews);
+        for (int i = 0; i < numReviews; i++) {
+            Review review = getReview(i);
+            reviews.add(review);
+            reviewMongoRepository.save(new ReviewDocument(review));
+        }
+        List<ReviewDocument> reviewDocuments = reviewMongoRepository.findAll();
+        assertThat(reviewDocuments.size()).isEqualTo(numReviews);
+    }
+
+    private Review getReview(int reviewNumber) {
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Product Name");
+        product.setDescription("Product description");
+        Review review = new Review();
+        review.setId((long) reviewNumber);
+        review.setTitle("Review " + reviewNumber + " title");
+        review.setContent("Review " + reviewNumber + " content");
+        review.setDatePosted(new Date());
+        review.setProduct(product);
+        return review;
+    }
 
 }
