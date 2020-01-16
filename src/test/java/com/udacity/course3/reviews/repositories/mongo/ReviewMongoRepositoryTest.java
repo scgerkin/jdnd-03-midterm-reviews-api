cf82b38ee21626dd79ac010ec92a20534f187548
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-01-16 17:30:04 -0500

    Basic Mongo template testing

--- src/test/java/com/udacity/course3/reviews/repositories/mongo/ReviewMongoRepositoryTest.java
+++ src/test/java/com/udacity/course3/reviews/repositories/mongo/ReviewMongoRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.udacity.course3.reviews.repositories.mongo;
 
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,4 +27,17 @@ public class ReviewMongoRepositoryTest {
         assertThat(reviewMongoRepository).isNotNull();
         assertThat(commentMongoRepository).isNotNull();
     }
+
+    @Test
+    public void testMongoTemplate() {
+        DBObject dbObject = BasicDBObjectBuilder.start()
+                                .add("key", "value")
+                                .get();
+        mongoTemplate.save(dbObject, "collectionName");
+        assertThat(mongoTemplate.findAll(DBObject.class, "collectionName"))
+            .extracting("key")
+            .containsOnly("value");
+    }
+
+
 }
